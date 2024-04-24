70efab023ed8bf5560292bb77e0887fc918cb438
nima <andarjaninima@gmail.com>
2024-04-24 12:17:05 +0330

    create task model

---
+++ app/Models/Task.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+
+        'task_subject',
+        'task_description',
+        'user_id',
+        'team_id',
+    ];
+}
