303daaf3bccd03ad6c2e09221af956d3fcc89639
nima <andarjaninima@gmail.com>
2024-04-24 11:57:28 +0330

    create factor controller

---
+++ app/Http/Controllers/FactorController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Factor;
+use GuzzleHttp\Promise\Create;
+use Illuminate\Http\Request;
+
+class FactorController extends Controller
+{
+    public function index($id = null)
+    {
+        if ($id) {
+            $factor = Factor::where('id', $id)->first();
+        } else {
+            $factor = Factor::orderby('id', 'desc')->get();
+        }
+        return response()->json($factor);
+    }
+
+    public function store(Request $request)
+    {
+        $factor = Factor::create($request->toArray());
+        return response()->json($factor);
+    }
+
+    public function edit(Request $request, $id)
+    {
+        $factor = Factor::where('id', $id)->update($request->toArray());
+        return response()->json($factor);
+    }
+
+    public function delete($id){
+        $factor = Factor::where('id', $id)->delete();
+        return response()->json($factor);
+    }
+}
