f3954eb4444077b767a5f68bf29480b4a03da5fe
nima <andarjaninima@gmail.com>
2024-04-24 12:00:48 +0330

    create note controller

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Note;
+use Illuminate\Http\Request;
+
+use function Laravel\Prompts\note;
+
+class NoteController extends Controller
+{
+    public function index($id = null)
+    {
+        if ($id) {
+            $note = Note::where('id', $id)->first();
+        } else {
+            $note = Note::orderby('id', 'desc')->get();
+        }
+        return response()->json($note);
+    }
+
+    public function store(Request $request){
+        $note = Note::create($request->toArray());
+        return response()->json($note);
+    }
+
+    public function edit(Request $request, $id){
+        $note = Note::where('id', $id)->update($request->toArray());
+        return response()->json($note);
+    }
+
+    public function delete($id){
+        $note = Note::where('id' , $id)->delete();
+        return response()->json($note);
+    }
+}
