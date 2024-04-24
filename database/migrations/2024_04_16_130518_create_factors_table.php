8f3a9830ac66c4969782c7779c79b899ec1eff60
nima <andarjaninima@gmail.com>
2024-04-24 12:26:07 +0330

    craete factor table

---
+++ database/migrations/2024_04_16_130518_create_factors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('factors', function (Blueprint $table) {
+            $table->id();
+            $table->integer('price');
+            $table->integer('number')->unique();
+            $table->string('seller_name');
+            $table->text('description');
+            $table->date('warrenty_started_at');
+            $table->date('warrenty_ended_at');
+            $table->unsignedBigInteger('order_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('factors');
+    }
+};
