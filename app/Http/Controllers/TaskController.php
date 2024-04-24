ccac98d942a261e063117269d92863d390e69129
nima <andarjaninima@gmail.com>
2024-04-24 12:04:04 +0330

    create task controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index($id = null)
+    {
+        if ($id) {
+            $task = Task::where('id', $id)->first();
+        } else {
+            $task = Task::orderby('id', 'desc')->get();
+        }
+        return response()->json($task);
+    }
+
+    public function store(Request $request)
+    {
+        $task = Task::create($request->toArray());
+        return response()->json($task);
+    }
+
+    public function edit(Request $request, $id)
+    {
+        $task = Task::where('id', $id)->update($request->toArray());
+        return response()->json($task);
+    }
+    public function delete($id){
+        $task = Task::where('id' , $id)->delete();
+        return response()->json($task);
+    }
+}
