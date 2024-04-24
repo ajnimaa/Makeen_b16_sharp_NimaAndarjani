3a78e7fe903268392e53bab794a04bffe53dbc26
nima <andarjaninima@gmail.com>
2024-04-24 12:11:44 +0330

    create request()validation file

---
+++ app/Http/Requests/OrderEditRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class OrderEditRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    // public function authorize(): bool
+    // {
+    //     return false;
+    // }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'order_name' => 'required',
+            'order_code' => 'required',
+            'order_delivery_time' => 'date|required',
+            'delivery_method' => 'required'
+        ];
+    }
+}
