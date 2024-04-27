59abe038dd9b174fbb01075b94dd5073cf5b4cd9
nima <andarjaninima@gmail.com>
2024-04-27 15:18:14 +0330

    edit user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,12 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -26,6 +31,7 @@ class User extends Authenticatable
         'email',
         'password',
         'gender',
+        'team_id',
     ];
 
     /**
@@ -52,6 +58,36 @@ class User extends Authenticatable
     public function orders(): HasMany
     {
         return $this->hasMany(Order::class);
+    }
+
+    public function taskable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    public function factorUser(): HasOneThrough
+    {
+        return $this->hasOneThrough(Factor::class, Order::class);
+    }
+
+    public function products(): HasManyThrough
+    {
+        return $this->hasManyThrough(Product::class, Order::class);
+    }
+
+    public function messages(): HasManyThrough
+    {
+        return $this->hasManyThrough(Massage::class, Ticket::class);
+    }
+
+    public function labels(): MorphToMany
+    {
+        return $this->morphToMany(Label::class, 'labelable');
+    }
+
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
     }
 }
 
