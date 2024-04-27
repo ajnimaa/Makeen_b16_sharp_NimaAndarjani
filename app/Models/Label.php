351bb19d9af5c3b3097f146534edd4ee56b8ddeb
nima <andarjaninima@gmail.com>
2024-04-27 15:16:07 +0330

    edit label model

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Label extends Model
 {
@@ -13,4 +14,20 @@ class Label extends Model
 
         'name'
     ];
+
+    public function users(): MorphToMany
+    {
+        return $this->morphedByMany(User::class, 'labelable');
+    }
+
+    public function teams(): MorphToMany
+    {
+        return $this->morphedByMany(Team::class, 'labelable');
+    }
+
+    public function products(): MorphToMany
+    {
+        return $this->morphedByMany(Product::class, 'labelable');
+    }
+
 }
