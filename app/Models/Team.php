c560fffd8b529870e5dad2119baaf4eaaa356032
nima <andarjaninima@gmail.com>
2024-05-08 23:09:15 +0330

    edit team model

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
@@ -22,8 +23,8 @@ class Team extends Model
         return $this->morphTo();
     }
 
-    public function users(): HasMany
+    public function users(): BelongsTo
     {
-        return $this->hasMany(User::class);
+        return $this->belongsTo(User::class);
     }
 }
