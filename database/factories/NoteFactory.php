f894e1cda580948ae8607cdbd83eb3389a8e161c
nima <andarjaninima@gmail.com>
2024-05-08 23:21:24 +0330

    create note factory

---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Note>
+ */
+class NoteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+                'note_subject' => fake()->word(),
+                'note_text' => fake()->text(),
+                'user_id' => User::factory()
+        ];
+    }
+}
