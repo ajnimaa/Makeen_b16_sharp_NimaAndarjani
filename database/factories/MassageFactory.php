9ff66e2ff23d86b85fc1028cae40b41a6d802cfa
nima <andarjaninima@gmail.com>
2024-05-08 23:20:49 +0330

    create massage factory

---
+++ database/factories/MassageFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Ticket;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Massage>
+ */
+class MassageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'description' => fake()->text(),
+            'ticket_id' => Ticket::factory(),
+            'subject' => fake()->word()
+        ];
+    }
+}
