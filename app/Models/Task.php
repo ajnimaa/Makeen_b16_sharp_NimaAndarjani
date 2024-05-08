48f66f9e5cc19934aa7f2e621d3042f83df6e526
nima <andarjaninima@gmail.com>
2024-05-08 23:08:12 +0330

    edit task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Task extends Model
@@ -21,5 +22,10 @@ class Task extends Model
     public function taskable(): MorphTo
     {
         return $this->morphTo();
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
