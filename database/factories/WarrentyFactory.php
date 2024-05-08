05cfd4c57e380506e708d2237e428b957998ad41
nima <andarjaninima@gmail.com>
2024-05-08 23:23:49 +0330

    create warrenty factory

---
+++ database/factories/WarrentyFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Warrenty>
+ */
+class WarrentyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'started_at' => fake()->date(),
+            'ended_at' => fake()->date()
+        ];
+    }
+}
