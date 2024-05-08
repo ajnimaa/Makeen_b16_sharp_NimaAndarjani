8c729c288ef30c6371fd3c06b7b97af982fc5746
nima <andarjaninima@gmail.com>
2024-05-08 23:21:52 +0330

    create order factory

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'order_name' => fake()->word(),
+            'order_code' => fake()->randomNumber(8 , true),
+            'order_delivery_time' => fake()->dateTime(),
+            'delivery_method' => fake()->randomElement(['in person' , 'not in person']),
+            'user_id' => User::factory(),
+            'product_id' => Product::factory()
+        ];
+    }
+}
