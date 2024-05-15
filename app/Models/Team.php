f9f26c19e33c41362b4f7d48ba8375e2aab6acb8
nima <andarjaninima@gmail.com>
2024-05-15 12:28:32 +0330

    change relation in team model

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
@@ -23,8 +24,8 @@ class Team extends Model
         return $this->morphTo();
     }
 
-    public function users(): BelongsTo
+    public function users(): BelongsToMany
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsToMany(User::class);
     }
 }
