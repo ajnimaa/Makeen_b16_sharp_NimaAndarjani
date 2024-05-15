84cb5e5d299661b04fce9c70c2f1e275298ca921
nima <andarjaninima@gmail.com>
2024-05-15 11:48:41 +0330

    write team user pvot table

--- database/migrations/2024_05_15_081453_create_team_users_table.php
+++ database/migrations/2024_05_15_081453_create_team_users_table.php
@@ -12,8 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('team_users', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->integer('team_id');
+            $table->integer('user_id');
+            $table->unique(['team_id' , 'user_id']);
         });
     }
 
