95abcef0d6c081e3620697aaf32ace5eb0e8c05c
nima <andarjaninima@gmail.com>
2024-05-18 15:16:19 +0330

    edit ticket model

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Ticket extends Model
 {
@@ -13,4 +14,9 @@ class Ticket extends Model
         'subject',
         'user_id',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
