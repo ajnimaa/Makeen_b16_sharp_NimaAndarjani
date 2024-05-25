3086c3c9bd68de84571c9721f2c3507cf32ce3ea
nima <andarjaninima@gmail.com>
2024-05-25 13:08:11 +0330

    edit media controller

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -4,22 +4,37 @@ namespace App\Http\Controllers;
 
 use App\Models\Media;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class MediaController extends Controller
 {
-    public function store(Request $request): string
+    public function index($id = null)
+    {
+        if($id){
+            $media = Media::with('users')->orderBy('id', 'desc')->paginate(10);
+        }else{
+            $media = Media::with('users')->find($id);
+        }
+        return response()->json($media);
+    }
+    public function storeprofile(Request $request): string
     {
         $path = $request->file('product image')->store('product images');
         return $path;
     }
-
-    public function index($id = null)
+    public function edit(Request $request, $id)
     {
-        if($id){
-            $path = Media::where($id, 'id')->first();
-        }else{
-            $path = Media::where($id, 'id')->get();
-        }
-        return $path;
+        $media = Media::where('id', $id)->update($request->toArray());
+        return response()->json($media);
+    }
+    public function delete($id)
+    {
+        $media = Media::where($id, 'id')->delete();
+        return response()->json($media);
+    }
+    public function download(Request $request)
+    {
+        $path = $request->path;
+        return Storage::download($path);
     }
 }
